4f582f2fd1238b69028ea6a2b1ce875a36f1c06c
sksoumik <sadmanks@gmail.com>
2022-01-13 20:18:37 +0600

    merge sorted array normal

---
+++ leetcode/merge_sorted_list_normal.py
@@ -0,0 +1,13 @@
+# Input: list1 = [1,2,4], list2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+
+def merge_sorted_array(list1, list2):
+    res = list1 + list2
+    sorted_res = sorted(res)
+    return sorted_res
+
+
+if __name__ == '__main__':
+    list1 = [1, 2, 4]
+    list2 = [1, 3, 4]
+    print(merge_sorted_array(list1, list2))
