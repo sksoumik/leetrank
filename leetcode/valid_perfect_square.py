7b8cbc5b7cbd7951cc47f41f905d50d7ee66da98
sksoumik <sadmanks@gmail.com>
2022-02-17 19:37:58 +0600

    valid perfect square

---
+++ leetcode/valid_perfect_square.py
@@ -0,0 +1,48 @@
+# https://leetcode.com/problems//valid-perfect-square/
+
+# Given a positive integer num, write a function which returns True if num is a perfect square else False.
+# Follow up: Do not use any built-in library function such as sqrt.
+
+# Input: num = 16
+# Output: true
+
+from typing import List
+import math
+
+from numpy import mat
+
+# approach: binary search
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if num == 1:
+            return True
+        left, right = 1, num
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == num:
+                return True
+            elif mid * mid < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
+
+
+# approach: built-in function
+# class Solution:
+#     def isPerfectSquare(self, num: int) -> bool:
+#         a = math.sqrt(num)
+#         return a == int(a)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.isPerfectSquare(16))
+    print(s.isPerfectSquare(14))
+    print(s.isPerfectSquare(1))
+    print(s.isPerfectSquare(2))
+    print(s.isPerfectSquare(3))
+    print(s.isPerfectSquare(4))
+    print(s.isPerfectSquare(5))
+    print(s.isPerfectSquare(6))
+    print(s.isPerfectSquare(7))
