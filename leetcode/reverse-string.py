f4c4d008d58f7b3bbe7f35d55a4bc57627b0a5d1
sksoumik <sadmanks@gmail.com>
2022-04-02 04:36:37 +0600

    reverse a string

--- leetcode/reverse-string.py
+++ leetcode/reverse-string.py
@@ -3,3 +3,17 @@
 # Write a function that reverses a string. The input string is given as an array of characters s.
 
 # You must do this by modifying the input array in-place with O(1) extra memory.
+
+from typing import List
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
+    
+
+if __name__ == '__main__':
+    s = Solution()
+    s.reverseString(["h","e","l","l","o"])
