50c4b4aff94a993efaedadeff3e2d540f346a611
sksoumik <sadmanks@gmail.com>
2022-04-02 01:12:49 +0600

    remove all non alphanumeric characters

--- leetcode/valid-palindrome.py
+++ leetcode/valid-palindrome.py
@@ -6,6 +6,7 @@ import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
+        # remove all non alphanumeric characters
         s = re.sub(r"[^a-zA-Z0-9]", "", s)
         return s == s[::-1]
 
