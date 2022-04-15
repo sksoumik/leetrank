6b37a1e34533c12b17aaf95ab83d24ef9f21be61
sksoumik <sadmanks@gmail.com>
2022-04-15 23:26:34 +0600

    contain duplicates

--- README.md
+++ README.md
@@ -44,3 +44,8 @@ Else
     - Map/Set for O(1) time & O(n) space
     - Sort input for O(nlogn) time and O(1) space
 ```
+
+#### Revise technique
+1. Assume you'are explaining the code to someone else and read it loud. Or just make a tutorial out of the code. 
+2. Read out each line of code explaining what it does. 
+
---
+++ leetcode/contains_duplicate.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/contains-duplicate
+
+# Given an integer array nums, return true if any value appears at least twice in
+# the array, and return false if every element is distinct.
+
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        unique_values = list(set(nums))
+
+        if len(unique_values) != len(nums):
+            return True
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.containsDuplicate([1, 2, 3, 1]))
+    print(sol.containsDuplicate([1, 2, 3, 4]))
+    print(sol.containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
