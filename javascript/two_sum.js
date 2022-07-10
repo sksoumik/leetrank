be11ff104619e6820e9dc5c798302402d4a69b60
sksoumik <sadmanks@gmail.com>
2022-07-10 23:11:02 +0600

    two sum js

---
+++ javascript/two_sum.js
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/binary-search/
+
+// Given an array of integers nums which is sorted in ascending order,
+// and an integer target, write a function to search target in nums.
+// If target exists, then return its index. Otherwise, return -1.
+// You must write an algorithm with O(log n) runtime complexity.
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    let hash_table = {};
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (complement in hash_table) {
+            return [hash_table[complement], i];
+        }
+        hash_table[nums[i]] = i;
+    }
+
+};
+
+
+console.log(twoSum([2, 7, 11, 15], 9));
