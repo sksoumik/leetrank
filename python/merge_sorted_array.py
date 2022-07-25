cb4c5c81473dfc4ac36103824c8da28e6214c98f
sksoumik <sadmanks@gmail.com>
2022-07-25 22:27:43 +0600

    merge sorted array code update

--- python/merge_sorted_array.py
+++ python/merge_sorted_array.py
@@ -22,18 +22,33 @@ from typing import List
 
 
 class Solution:
+    # easy solution but might not work for coding interviews
+    # def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    #
+    #     nums1[m:] = nums2[:n]
+    #     nums1.sort()
+
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
 
-        nums1[:] = nums1[:m]
-
-        for idx, num in enumerate(nums2):
-            nums1.append(num)
-
-        nums1.sort()
-        nums1 = nums1[: m + n]
+        # start merging from the ends of the lists
+        p1, p2 = m - 1, n - 1
+        last_idx = m + n - 1
+        # destination d index to insert into nums1
+        for d in range(last_idx, -1, -1):
+            # if second list is empty, nothing more to merge
+            if p2 < 0:
+                return
+            # only merge from nums1 if there are items left to merge
+            # insert at d the larger of the two values from nums1 and nums2
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[d] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[d] = nums2[p2]
+                p2 -= 1
 
 
 if __name__ == "__main__":
