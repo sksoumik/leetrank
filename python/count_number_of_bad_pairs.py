a64993f51b49a37d1d8092e20d111d08725d64b3
sksoumik <sadmanks@gmail.com>
2022-08-08 22:28:14 +0600

    merge intervals

---
+++ python/count_number_of_bad_pairs.py
@@ -0,0 +1,53 @@
+# https://leetcode.com/count-number-of-bad-pairs
+
+# You are given a 0-indexed integer array nums.
+# A pair of indices (i, j) is a bad pair if i < j and j - i != nums[j] - nums[i].
+
+# Return the total number of bad pairs in nums.
+
+# Input: nums = [4,1,3,3]
+# Output: 5
+
+from typing import List
+
+from collections import Counter
+
+
+class Solution:
+
+    # brute force
+    def countBadPairs_bf(self, nums):
+        count = 0
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if i < j and nums[j] - nums[i] != j - i:
+                    count += 1
+        return count
+
+    def countBadPairs(self, nums):
+        length = len(nums)
+        result = []
+
+        for i in range(length):
+            result.append(nums[i] - i)
+        
+        print(result)
+        counter = Counter(result)
+        print(counter)
+
+        answer = length * (length - 1) // 2
+
+        print(answer)
+
+        for x in counter:
+            answer -= counter[x] * (counter[x] - 1) // 2
+        return answer
+
+
+if __name__ == "__main__":
+    nums = [4, 1, 3, 3]
+    s = Solution()
+    print(s.countBadPairs(nums))
+
+    # nums = [1, 2, 3, 4, 5]
+    # print(s.countBadPairs(nums))
---
+++ python/merge_intervals.py
@@ -0,0 +1,47 @@
+# https://leetcode.com/problems/merge-intervals/
+
+# Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, 
+# and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+ 
+
+# Example 1:
+
+# Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+# Output: [[1,6],[8,10],[15,18]]
+# Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+
+from typing import List
+
+class Solution:
+    # go through the intervals sorted by start coordinate and either combine the current 
+    # interval with the previous one if they overlap, or add it to the output by itself if they don't.
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        if len(intervals) == 0:
+            return []
+
+        # sort by start coordinate of intervals
+        intervals.sort(key=lambda x: x[0])
+        
+        output = [intervals[0]]
+        for i in range(1, len(intervals)):
+            
+            # intervals[i][0]  means the start/first coordinate of the current array
+            # output[-1][1] means the end/last coordinate of the last array in the output
+            if intervals[i][0] <= output[-1][1]: 
+                # intervals[i][1]  means the end/last/second coordinate of the current array
+                output[-1][1] = max(output[-1][1], intervals[i][1])
+            else:
+                output.append(intervals[i])
+        return output
+            
+
+    
+if __name__ == '__main__':
+    intervals = [[1,3],[2,6],[8,10],[15,18]]
+    sol = Solution()
+    print(sol.merge(intervals))
+
+
+        
