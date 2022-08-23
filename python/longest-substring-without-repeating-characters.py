70be8653e2ff542064a57f84d457bf7876fa5dbb
sksoumik <sadmanks@gmail.com>
2022-08-24 04:57:00 +0600

    longest substring without repeating characters add bf

--- python/longest-substring-without-repeating-characters.py
+++ python/longest-substring-without-repeating-characters.py
@@ -8,6 +8,7 @@
 # Output: 3
 # Explanation: The answer is "abc", with the length of 3.
 
+
 # code explanation: https://www.youtube.com/watch?v=wiGpQwVHdE0
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
@@ -47,8 +48,30 @@ class Solution:
             max_len = max(max_len, len(char_set))
         return max_len
 
+    # brute force approach: time O(n^2)
+    # time limit exceeded
+    def lengthOfLongestSubstring2(self, s: str) -> int:
+        # create all possible substrings
+        all_substrings = []
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                all_substrings.append(s[i : j + 1])
+
+        # print(all_substrings)
+
+        # all substrings with distinct characters
+        all_substrings_distinct = []
+        for substring in all_substrings:
+            if len(set(substring)) == len(substring):
+                all_substrings_distinct.append(substring)
+        # longest substring with distinct characters
+        longest_substring = max(all_substrings_distinct, key=len)
+        return len(longest_substring)
+
 
 if __name__ == "__main__":
     sol = Solution()
     print(sol.lengthOfLongestSubstring("abcabcbb"))
+    print(sol.lengthOfLongestSubstring("bbbbb"))
+    print(sol.lengthOfLongestSubstring2("abcabcbb"))
     print(sol.lengthOfLongestSubstring("bbbbb"))
