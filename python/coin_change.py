ec6d382f6d9589d6d7635ea3f4ae6f8e7fa6f7fe
sksoumik <sadmanks@gmail.com>
2022-09-12 23:19:22 +0600

    add dfs method to coin change

--- python/coin_change.py
+++ python/coin_change.py
@@ -11,12 +11,13 @@
 # Input: coins = [1,2,5], amount = 11
 # Output: 3
 # Explanation: 11 = 5 + 5 + 1
-# Example 2:
 
+
+# Example 2:
 # Input: coins = [2], amount = 3
 # Output: -1
-# Example 3:
 
+# Example 3:
 # Input: coins = [1], amount = 0
 # Output: 0
 
@@ -42,9 +43,35 @@ class Solution:
         ans = dp(amount)
         return ans if ans != math.inf else -1
 
+    # dfs
+    def _coinChange(self, coins: List[int], amount: int) -> int:
+        result = []
+        # @lru_cache(None)
+        def dfs(current_combination, current_sum, idx):
+            if current_sum == amount:
+                result.append(current_combination)
+                return
+
+            elif current_sum > amount:
+                return
+
+            for i in range(idx, len(coins)):
+                dfs(current_combination + [coins[i]], current_sum + coins[i], i)
+
+        dfs([], 0, 0)
+
+        if not result:
+            return -1
+
+        min_length_array = min(result, key=len)
+
+        return len(min_length_array)
+
 
 if __name__ == "__main__":
     s = Solution()
     print(s.coinChange([1, 2, 5], 11))  # 3
     print(s.coinChange([2], 3))  # -1
     print(s.coinChange([1], 0))  # 0
+
+    print(s._coinChange([1, 2, 5], 11))  # 3
