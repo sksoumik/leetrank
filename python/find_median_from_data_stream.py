156e9b54a5346cbd52a47aa31dd268f66018d6c2
sksoumik <sadmanks@gmail.com>
2022-09-20 08:41:55 +0600

    find median from data stream

---
+++ python/find_median_from_data_stream.py
@@ -0,0 +1,56 @@
+# https://leetcode.com/problems/find-median-from-data-stream/
+
+# The median is the middle value in an ordered integer list.
+# If the size of the list is even, there is no middle value and the median is the mean of the two middle values.
+
+# For example, for arr = [2,3,4], the median is 3.
+# For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+# Implement the MedianFinder class:
+
+# MedianFinder() initializes the MedianFinder object.
+# void addNum(int num) adds the integer num from the data stream to the data structure.
+# double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+
+
+# Example 1:
+
+# Input
+# ["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+# [[], [1], [2], [], [3], []]
+# Output
+# [null, null, null, 1.5, null, 2.0]
+
+# Explanation
+# MedianFinder medianFinder = new MedianFinder();
+# medianFinder.addNum(1);    // arr = [1]
+# medianFinder.addNum(2);    // arr = [1, 2]
+# medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+# medianFinder.addNum(3);    // arr[1, 2, 3]
+# medianFinder.findMedian(); // return 2.0
+
+
+class MedianFinder:
+    def __init__(self):
+        self.data = []
+
+    def addNum(self, num: int) -> None:
+        self.data.append(num)
+
+    def findMedian(self) -> float:
+        self.data.sort()
+        mid_index = len(self.data) // 2
+        # if the list length is odd
+        if len(self.data) % 2 != 0:
+            return self.data[mid_index]
+        # if the list length is even
+        else:
+            return (self.data[mid_index - 1] + self.data[mid_index]) / 2
+
+
+if __name__ == "__main__":
+    mf = MedianFinder()
+    mf.addNum(1)
+    mf.addNum(2)
+    print(mf.findMedian())
+    mf.addNum(3)
+    print(mf.findMedian())
--- python/jump_game.py
+++ python/jump_game.py
@@ -10,44 +10,26 @@
 # Output: true
 # Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
 
+#  video explanation: https://youtu.be/EgMPjWliYGY
 
 from typing import List
 
 
 class Solution:
-    # https://youtu.be/Yan0cv2cLy8?t=606
+    # greedy approach
+    # time complexity: O(n)
     def canJump(self, nums: List[int]) -> bool:
-        """
-        greedy approach : O (n)
-        we start from the end of the array
-        if we can reach the end index from its previous index
-        then we update the goal to its previous index and then look again
-        wheather we can reach to the updated goal from its previous index
-        we continue this and if we can make the goal to reach at index 0
-        then we return True
-        else False
-        """
         goal = len(nums) - 1  # last index
-        # iterate the array backwards
+
+        # for i in reversed(range(goal)):
         for idx in range(goal, -1, -1):
             if idx + nums[idx] >= goal:
+                # we have added the current index to the jump length,
+                # because at any given index, futhest we can reach is the current index + jump length
                 goal = idx
 
         # return True if goal == 0 else False
         return goal == 0
-
-    # second approach
-    # time complexity O(n)
-    # video explanation: https://youtu.be/muDPTDrpS28
-    def _canJump(self, nums: List[int]) -> bool:
-        reachable = 0
-
-        for idx, num in enumerate(nums):
-            if idx > reachable:
-                return False
-            reachable = max(reachable, idx + num)
-
-        return True
 
 
 if __name__ == "__main__":
