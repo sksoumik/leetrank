1873a41124b329bc5f2fc830abc6d90e8a71f976
sksoumik <sadmanks@gmail.com>
2022-10-01 16:32:35 +0600

    subarray sum equals k update

--- python/subarray_sum_equals_k.py
+++ python/subarray_sum_equals_k.py
@@ -26,40 +26,20 @@ class Solution:
                     count += 1
         return count
 
-    
-    # solution 2: time O(n), space O(n)
-    # First of all, the basic idea behind this code is that, whenever sums has 
-    # increased by a value of k, we've found a subarray of sums=k.
-    # I'll also explain why we need to initialise 0 in the hashmap.
-    # Example: Let's say our elements are [1,2,1,3] and k = 3.
-    # and our corresponding running sums = [1,3,4,7]
-    # Now, if you notice the running sums array, from 1->4, there is increase of k and from 4->7, 
-    # there is an increase of k. So, we've found 2 subarrays of sums=k.
-
-    # But, if you look at the original array, there are 3 subarrays of sums==k. 
-    # Now, you'll understand why 0 comes in the picture.
-
-    # In the above example, 4-1=k and 7-4=k. Hence, we concluded that there are 2 subarrays.
-    # However, if sums==k, it should've been 3-0=k. But 0 is not present in the array. 
-    # To account for this case, we include the 0.
-    # Now the modified sums array will look like [0,1,3,4,7]. Now, try to see for the increase of k.
-
-    # 0->3
-    # 1->4
-    # 4->7
-    # Hence, 3 sub arrays of sums=k
-
+    # time complexity: O(n)
+    # space complexity: O(n)
+    # vid: https://youtu.be/r1cwGocurtA
     def subarraySum2(self, nums: List[int], k: int) -> int:
-
         count = 0
         sums = 0
-        running_sum = dict()
-        running_sum[0] = 1
+        d = {0: 1} # {running_sum: frequency}
 
         for num in nums:
             sums += num
-            count += running_sum.get(sums - k, 0)
-            running_sum[sums] = running_sum.get(sums, 0) + 1
+            if sums - k in d:
+                count += d[sums - k]
+            d[sums] = d.get(sums, 0) + 1
+
         return count
 
 
