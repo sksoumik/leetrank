7ddadca5f4e52390ad850db64e2e5d3eb33534d2
sksoumik <sadmanks@gmail.com>
2022-10-11 22:24:56 +0600

    palindrome partioning add lru cache

--- python/palindrome_partitioning.py
+++ python/palindrome_partitioning.py
@@ -14,6 +14,7 @@
 # vid: https://youtu.be/xhEEpnn0x1U
 
 from typing import List
+from functools import lru_cache
 
 
 class Solution:
@@ -34,6 +35,27 @@ class Solution:
                     dfs(i + 1, sofar + [s[start : i + 1]])
 
         dfs(0, [])
+        return output
+
+    # using lru_cache
+    def _partition(self, s: str) -> List[List[str]]:
+        # use tuple instead of list
+        n = len(s)
+        output = []
+
+        @lru_cache(None)
+        def dfs(start: int, sofar: tuple):
+            # base case
+            if start == n:
+                output.append(sofar)
+                return
+
+            for i in range(start, n):
+                # check for palindromes
+                if s[start : i + 1] == s[start : i + 1][::-1]:
+                    dfs(i + 1, sofar + (s[start : i + 1],))
+
+        dfs(0, ())
         return output
 
 
