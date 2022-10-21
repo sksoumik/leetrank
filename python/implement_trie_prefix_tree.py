659f8b38b6507f2dbd4e8f08cfcf626bdb158097
sksoumik <sadmanks@gmail.com>
2022-10-22 03:45:44 +0600

    update Trie

--- python/implement_trie_prefix_tree.py
+++ python/implement_trie_prefix_tree.py
@@ -1,16 +1,16 @@
 # https://leetcode.com/problems/implement-trie-prefix-tree/
 
-# A trie (pronounced as "try") or prefix tree is a tree data structure used to 
-# efficiently store and retrieve keys in a dataset of strings. There are various 
+# A trie (pronounced as "try") or prefix tree is a tree data structure used to
+# efficiently store and retrieve keys in a dataset of strings. There are various
 # applications of this data structure, such as autocomplete and spellchecker.
 
 # Implement the Trie class:
 
 # Trie() Initializes the trie object.
 # void insert(String word) Inserts the string word into the trie.
-# boolean search(String word) Returns true if the string word is in the trie 
+# boolean search(String word) Returns true if the string word is in the trie
 # (i.e., was inserted before), and false otherwise.
-# boolean startsWith(String prefix) Returns true if there is a previously inserted 
+# boolean startsWith(String prefix) Returns true if there is a previously inserted
 # string word that has the prefix prefix, and false otherwise.
 
 # Input
@@ -45,11 +45,11 @@ class Trie:
 
     def search(self, word: str) -> bool:
         """
-        We start from the root node and traverse the trie until we reach the end of the word. 
-        
+        We start from the root node and traverse the trie until we reach the end of the word.
+
         If we reach the end of the word and the last node we visited is a leaf node, then the word
-        exists in the trie. 
-        
+        exists in the trie.
+
         If we reach the end of the word and the last node we visited is not a leaf node, then the word
         does not exist in the trie.
         """
@@ -61,6 +61,12 @@ class Trie:
         return "#" in node
 
     def startsWith(self, prefix: str) -> bool:
+        """
+        We start from the root node and traverse the trie until we reach the end of the prefix.
+        In this case, we don't need to check if the last node is a #, because we are not
+        looking for a complete word.
+
+        """
         node = self.root
         for char in prefix:
             if char not in node:
@@ -77,5 +83,3 @@ if __name__ == "__main__":
     print(trie.startsWith("app"))  # True
     trie.insert("app")
     print(trie.search("app"))  # True
-
-
