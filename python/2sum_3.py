6a259fb5f7749b72c9fab3a98d4c46331658bc40
sksoumik <sadmanks@gmail.com>
2022-11-02 01:44:22 +0600

    add naive solution for two sum 3

--- python/2sum_3.py
+++ python/2sum_3.py
@@ -14,7 +14,32 @@
 
 import collections
 
+# naive implementation
+class TwoSum:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.nums = []
 
+    def add(self, number: int) -> None:
+        """
+        Add the number to an internal data structure..
+        """
+        self.nums.append(number)
+
+    def find(self, value: int) -> bool:
+        """
+        Find if there exists any pair of numbers which sum is equal to the value.
+        """
+        for i in range(len(self.nums)):
+            for j in range(i + 1, len(self.nums)):
+                if self.nums[i] + self.nums[j] == value:
+                    return True
+        return False
+
+
+# optimized implementation using hash table
 class TwoSum:
     def __init__(self):
         """
