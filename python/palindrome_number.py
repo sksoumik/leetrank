1b00d7f7c5b0bbf4ca7fbecc8b37a55787e91c43
sksoumik <sadmanks@gmail.com>
2022-12-02 14:40:22 +0600

    add palindrome without str

--- python/palindrome_number.py
+++ python/palindrome_number.py
@@ -9,7 +9,25 @@ def is_palindrome(x: int):
     reverse_x = str_x[::-1]
     return str_x == reverse_x
 
+#without converting to string
+def is_palindrome2(x: int):
+    # any negative number is not a palindrome
+    if x < 0:
+        return False
+
+    reverse = 0
+    original = x
+    while x > 0:
+        # Adding the last digit of x to the reverse variable.
+        reverse = reverse * 10 + x % 10
+        # The same as `x = x // 10`
+        x = x // 10
+
+    return reverse == original
+
+
 
 if __name__ == "__main__":
-    print(is_palindrome(121))
-    print(is_palindrome(123))
+    # print(is_palindrome(121))
+    # print(is_palindrome(123))
+    print(is_palindrome2(121))
