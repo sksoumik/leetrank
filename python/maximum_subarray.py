4094719d392b8505c7958a431dfc25ac0250b64b
Sadman Kabir Soumik <sadmanks@gmail.com>
2023-03-08 09:58:05 +0600

    add brute force solution

--- python/maximum_subarray.py
+++ python/maximum_subarray.py
@@ -3,8 +3,10 @@
 # Output: 6
 # Explanation: [4,-1,2,1] has the largest sum = 6.
 
+from typing import List
 
 # brute force
+# time complexity: O(n^2)
 def bf_max_subarray(nums):
     """
     We check for all possible subarrays and find it's sum and update the maximum sum
@@ -22,35 +24,8 @@ def bf_max_subarray(nums):
     return max_sum
 
 
-
-# approach 1
-def fn_max_subarray(nums):
-    """
-    We keep track of the current sum and the maximum sum. If the current sum is greater than the maximum
-    sum, we update the maximum sum. If the current sum is less than 0, we reset the current sum to 0
-
-    :param nums: the list of numbers
-    :return: The maximum sum of a subarray.
-    """
-    max_sum = float("-inf")
-    current_sum = 0
-
-    for idx, item in enumerate(nums):
-        current_sum += item
-
-        if current_sum > max_sum:
-            max_sum = current_sum
-
-        if current_sum < 0:
-            current_sum = 0
-
-    return max_sum
-
-
-from typing import List
-
-
 class Solution:
+    # Kadane's Algorithm
     # video explanation: https://youtu.be/6HntYGZyjZI
     def maxSubArray(self, nums: List[int]) -> int:
         current_sum = maximum_sum = nums[0]
@@ -61,28 +36,8 @@ class Solution:
 
         return maximum_sum
 
-    # brute force
-    # time complexity O(n^2)
-    # check for all possible subarrays and find it's sum and update the maximum sum
-    def _maxSubArray(self, nums: List[int]) -> int:
-        max_sum = float("-inf")
-
-        for i in range(len(nums)):
-            current_sum = 0
-            for j in range(i, len(nums)):
-                current_sum += nums[j]
-                max_sum = max(max_sum, current_sum)
-
-        return max_sum
-
 
 if __name__ == "__main__":
     arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-    print(fn_max_subarray(arr))
+    print(bf_max_subarray(arr))
     print(Solution().maxSubArray(arr))
-
-    # arr2 = [1]
-    # print(fn_max_subarray(arr2))
-
-    # arr3 = [-2, -1]
-    # print(fn_max_subarray(arr3))
